23d2d722372845d566e5eda20db3fed34c647953
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2020-08-09 13:41:12 +0900

    Add test for `User::fetch_details`

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -19,6 +19,17 @@ impl User {
         }
     }
     /// Fetches the details of this user.
+    /// ```
+    /// # use niconico::*;
+    /// # const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+    /// # #[tokio::main]
+    /// # async fn main() {
+    /// # let mut session = Session::new(USER_AGENT, Language::Japanese);
+    /// # session.set_cookie_user_session(env!("NICO_SID"));
+    /// let login_user_details = User::LoginUser.fetch_details(&session).await.unwrap();
+    /// let user_1_details = User::UserId(1).fetch_details(&session).await.unwrap();
+    /// # }
+    /// ```
     pub async fn fetch_details(self, session: &Session) -> Result<details::UserDetails> {
         details::UserDetails::fetch(session, self).await
     }
